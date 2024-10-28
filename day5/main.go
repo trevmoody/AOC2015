ae78ca3ba71306932542c4fd01494ebb42b5ec5b
Trevor Moody <trevor.moody@gmail.com>
2024-10-28 17:56:08 +0000

    Day 5 part 2

--- day5/main.go
+++ day5/main.go
@@ -8,14 +8,59 @@ import (
 
 func main() {
 
-	part1((*util.GetFileAsLines("./day5/input")))
+	//	part1((*util.GetFileAsLines("./day5/input")))
+	part2((*util.GetFileAsLines("./day5/input")))
 
+}
+
+func part2(list []string) {
+	count := 0
+	for _, s := range list {
+		if checkString2(s) {
+			count = count + 1
+		}
+	}
+	fmt.Printf("part2 count = %d\n", count)
+}
+
+func checkString2(stringToCheck string) bool {
+
+	if twoLettersAppearTwice(stringToCheck) && repeatWithOneLetterBetween(stringToCheck) {
+		return true
+	}
+	return false
+}
+
+func repeatWithOneLetterBetween(input string) bool {
+	for i := 0; i < len(input)-2; i++ {
+		if input[i] == input[i+2] {
+			fmt.Printf("found 2 chars with one letter between %c %c %c\n", input[i], input[i+1], input[i+2])
+			return true
+		}
+	}
+	return false
+}
+
+func twoLettersAppearTwice(input string) bool {
+	inputLength := len(input) - 2
+	fmt.Printf("input length = %d\n", inputLength)
+	for i := 0; i < inputLength; i++ {
+		for j := i + 2; j < len(input)-1; j++ {
+			fmt.Printf("i = %d j = %d\n", i, j)
+			if input[i] == input[j] && input[i+1] == input[j+1] {
+				fmt.Printf("found 2 chars twice in a row %c%c %c%c\n", input[i], input[i+1], input[j], input[j+1])
+				return true
+			}
+		}
+
+	}
+	return false
 }
 
 func part1(list []string) {
 	count := 0
 	for _, s := range list {
-		if checkString(s) {
+		if checkString1(s) {
 			count = count + 1
 		}
 	}
@@ -23,7 +68,7 @@ func part1(list []string) {
 
 }
 
-func checkString(stringtocheck string) bool {
+func checkString1(stringToCheck string) bool {
 
 	// Initialize an empty set
 	doNotContain := make(map[string]struct{})
@@ -34,7 +79,7 @@ func checkString(stringtocheck string) bool {
 	doNotContain["pq"] = struct{}{}
 	doNotContain["xy"] = struct{}{}
 
-	if countVowels(stringtocheck) > 2 && checkIf2CharsInARow(stringtocheck) && doesNotContain(doNotContain, stringtocheck) {
+	if countVowels(stringToCheck) > 2 && checkIf2CharsInARow(stringToCheck) && doesNotContain(doNotContain, stringToCheck) {
 		return true
 	}
 
